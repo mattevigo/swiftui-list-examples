e97b8cf0916bbb54aab7e9156bdae773549c58eb
Matteo Vigoni <mattevigo@biobeats.com>
2020-11-19 15:55:09 +0100

    Add a list with content

--- swiftui-list-examples/swiftui-list-examples/InfiniteScrollingList.swift
+++ swiftui-list-examples/swiftui-list-examples/InfiniteScrollingList.swift
@@ -7,9 +7,42 @@
 
 import SwiftUI
 
+class InfiniteScrollingViewModel: ObservableObject {
+    var currentPage = 0
+    let pageLimit = 100
+
+    @Published var contents:[String] = []
+
+    func loadNextPage() {
+        contents.append(contentsOf: (0..<pageLimit).map { _ in
+            UUID().uuidString
+        })
+        currentPage = currentPage + 1
+        print("Page loaded: \(currentPage)")
+    }
+}
+
 struct InfiniteScrollingList: View {
+    @ObservedObject var viewModel = InfiniteScrollingViewModel()
+
     var body: some View {
-        Text("Inifinite scrolling")
+        List(viewModel.contents.indices, id: \.self) { index in
+            Item(id: index, content: viewModel.contents[index])
+        }
+    }
+
+    init() {
+        viewModel.loadNextPage()
+    }
+}
+
+struct Item: View, Identifiable {
+
+    var id: Int
+    var content: String
+
+    var body: some View {
+        Text("\(id) - \(String(content.prefix(6)))")
     }
 }
 
