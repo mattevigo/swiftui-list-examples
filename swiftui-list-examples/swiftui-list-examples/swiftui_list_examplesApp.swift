76ebe3c9a3b1bf0f3b630ce339c6d57e7e1b58ae
Matteo Vigoni <mattevigo@biobeats.com>
2020-11-19 14:44:37 +0100

    Create project

---
+++ swiftui-list-examples/swiftui-list-examples/swiftui_list_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_list_examplesApp.swift
+//  swiftui-list-examples
+//
+//  Created by Matteo Vigoni on 19/11/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_list_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui-list-examples/swiftui-list-examplesTests/swiftui_list_examplesTests.swift
@@ -0,0 +1,33 @@
+//
+//  swiftui_list_examplesTests.swift
+//  swiftui-list-examplesTests
+//
+//  Created by Matteo Vigoni on 19/11/20.
+//
+
+import XCTest
+@testable import swiftui_list_examples
+
+class swiftui_list_examplesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
