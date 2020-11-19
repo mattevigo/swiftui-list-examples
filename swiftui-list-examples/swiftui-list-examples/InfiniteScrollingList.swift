0cb1121433db059ffad9b29a824c8db6b797a254
Matteo Vigoni <mattevigo@biobeats.com>
2020-11-19 14:56:27 +0100

    Project structure

---
+++ swiftui-list-examples/swiftui-list-examples/InfiniteScrollingList.swift
@@ -0,0 +1,20 @@
+//
+//  InfinitScrollingList.swift
+//  swiftui-list-examples
+//
+//  Created by Matteo Vigoni on 19/11/20.
+//
+
+import SwiftUI
+
+struct InfiniteScrollingList: View {
+    var body: some View {
+        Text("Inifinite scrolling")
+    }
+}
+
+struct InfiniteScrollingList_Previews: PreviewProvider {
+    static var previews: some View {
+        InfiniteScrollingList()
+    }
+}
